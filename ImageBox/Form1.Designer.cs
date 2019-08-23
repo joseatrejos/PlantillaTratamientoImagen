d864d27544228e59af28126eb5e2f685ac92a3b1
Jose Alberto Trejo Serrano <joseatrejos@hotmail.com>
2019-08-23 01:43:14 -0700

    Agregar archivos de proyecto.

---
+++ ImageBox/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace ImageBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.imageBox2 = new Emgu.CV.UI.ImageBox();
+            this.histogramBox1 = new Emgu.CV.UI.HistogramBox();
+            this.histogramBox2 = new Emgu.CV.UI.HistogramBox();
+            this.histogramBox3 = new Emgu.CV.UI.HistogramBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Cargar Imagen";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(263, 23);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Escala de Grises";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(844, 22);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(110, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Histograma Azul";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(457, 22);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(119, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Histograma Rojo";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(633, 22);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(132, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Histograma Verde";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(13, 73);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(365, 240);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // imageBox2
+            // 
+            this.imageBox2.Location = new System.Drawing.Point(13, 347);
+            this.imageBox2.Name = "imageBox2";
+            this.imageBox2.Size = new System.Drawing.Size(365, 240);
+            this.imageBox2.TabIndex = 2;
+            this.imageBox2.TabStop = false;
+            // 
+            // histogramBox1
+            // 
+            this.histogramBox1.Location = new System.Drawing.Point(457, 417);
+            this.histogramBox1.Name = "histogramBox1";
+            this.histogramBox1.Size = new System.Drawing.Size(446, 126);
+            this.histogramBox1.TabIndex = 5;
+            // 
+            // histogramBox2
+            // 
+            this.histogramBox2.Location = new System.Drawing.Point(457, 90);
+            this.histogramBox2.Name = "histogramBox2";
+            this.histogramBox2.Size = new System.Drawing.Size(446, 126);
+            this.histogramBox2.TabIndex = 6;
+            // 
+            // histogramBox3
+            // 
+            this.histogramBox3.Location = new System.Drawing.Point(457, 268);
+            this.histogramBox3.Name = "histogramBox3";
+            this.histogramBox3.Size = new System.Drawing.Size(446, 126);
+            this.histogramBox3.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1002, 749);
+            this.Controls.Add(this.histogramBox3);
+            this.Controls.Add(this.histogramBox2);
+            this.Controls.Add(this.histogramBox1);
+            this.Controls.Add(this.imageBox2);
+            this.Controls.Add(this.imageBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private Emgu.CV.UI.ImageBox imageBox2;
+        private Emgu.CV.UI.HistogramBox histogramBox1;
+        private Emgu.CV.UI.HistogramBox histogramBox2;
+        private Emgu.CV.UI.HistogramBox histogramBox3;
+    }
+}
+
---
+++ ImageBox/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace ImageBox
+{
+    public partial class Form1 : Form
+    {
+        Image<Bgr, byte> _InputColor;
+        Image<Gray, byte> _InputGray;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            string imagen = "C:\\Users\\josea\\source\\repos\\PlantillaTratamientoImagen\\ImageBox\\img\\lena.jpg";
+            _InputColor = new Image<Bgr, byte>(imagen);
+
+            if (_InputColor == null)
+            {
+                MessageBox.Show("No se cargo la imagen");
+                return;
+            }
+
+            imageBox1.Image = _InputColor;
+            // Desactivar click derecho en el programa corriendo.
+            imageBox1.FunctionalMode = Emgu.CV.UI.ImageBox.FunctionalModeOption.Minimum;
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            // Convertir escala de grises.
+            _InputGray = _InputColor.Convert<Gray, byte>();
+            imageBox2.Image = _InputGray;
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            //inicializar variable para determinar rango de color
+            DenseHistogram histcolor = new DenseHistogram(256, new RangeF(0, 255));
+            histcolor.Calculate(new Image<Gray, Byte>[] { _InputColor[0] }, false, null);
+
+            Mat m = new Mat();
+            histcolor.CopyTo(m);
+
+            histogramBox1.AddHistogram("Histograma del Color azul", Color.Blue, m, 256, new float[] { 0, 256 });
+            histogramBox1.Refresh();
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            //inicializar variable para determinar rango de color
+            DenseHistogram histcolor = new DenseHistogram(256, new RangeF(0, 255));
+            histcolor.Calculate(new Image<Gray, Byte>[] { _InputColor[2] }, false, null);
+
+            Mat m = new Mat();
+            histcolor.CopyTo(m);
+
+            histogramBox2.AddHistogram("Histograma del Color rojo", Color.Red, m, 256, new float[] { 0, 256 });
+            histogramBox2.Refresh();
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            //inicializar variable para determinar rango de color
+            DenseHistogram histcolor = new DenseHistogram(256, new RangeF(0, 255));
+            histcolor.Calculate(new Image<Gray, Byte>[] { _InputColor[1] }, false, null);
+
+            Mat m = new Mat();
+            histcolor.CopyTo(m);
+
+            histogramBox3.AddHistogram("Histograma del Color verde", Color.Green, m, 256, new float[] { 0, 256 });
+            histogramBox3.Refresh();
+        }
+    }
+}
